e94d35f4c5895cc93f08dafccfe72a79825ce0df
Guilherme Avila <106833885+TerryKrom@users.noreply.github.com>
2023-06-27 18:41:51 -0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -3,7 +3,7 @@ const cardsContainer = document.querySelector('#movie-list');
 const cardsTop = document.querySelector('#movie-search')
 let i = 0;
 const showMovies = (query, space, qtd) => {
-  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&type=movie&plot=full&page=1`)
+  fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&type=movie&plot=full&page=1`)
 .then(response => response.json())
   .then(data => {
     if (Array.isArray(data.Search)) {
@@ -29,7 +29,7 @@ const showMovies = (query, space, qtd) => {
         rating.textContent = movie.imdbRating ? `Rating: ${movie.imdbRating}` : 'No rating available';
         cardBody.appendChild(rating);
         
-        const heartIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16" id=${i}>
+        const heartIcon = `<svg xmlns="https://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16" id=${i}>
         <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
         </svg>
       `;
