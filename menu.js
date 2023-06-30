317ddaf7acbd88a5a70272ce620f0110275894c2
Guilherme Avila <106833885+TerryKrom@users.noreply.github.com>
2023-06-30 14:05:47 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const burgerMenu = document.getElementById('burger-menu');
 const navLinks = document.querySelector('nav ul');
 const menuHide = document.querySelector('.menu-hide')
 const menu = document.querySelector('.menu-perfil')
+
 const showOutMenu = () => {
   if(localStorage.length != 0){
     menu.style.opacity=1;
