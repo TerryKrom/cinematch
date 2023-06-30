d17809e568369c1406565cdcb97104f06b8694ac
Guilherme Avila <106833885+TerryKrom@users.noreply.github.com>
2023-06-30 14:05:01 -0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -3,13 +3,13 @@ const cardsContainer = document.querySelector('#movie-list');
 const cardsTop = document.querySelector('#movie-search')
 let i = 0;
 const showMovies = (query, space, qtd) => {
-  fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&type=movie&plot=full&page=1`)
+  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&type=movie&plot=full&page=1`)
 .then(response => response.json())
   .then(data => {
     if (Array.isArray(data.Search)) {
       data.Search.slice(0,qtd).forEach(movie => {
         const card = document.createElement('div');
-        card.className = 'card col-12 col-sm-6 col-md-3 col-lg-3';
+        card.className = 'card col-12 col-sm-12 col-md-3 col-lg-3';
         
         const poster = document.createElement('img');
         poster.src = movie.Poster;
@@ -29,7 +29,7 @@ const showMovies = (query, space, qtd) => {
         rating.textContent = movie.imdbRating ? `Rating: ${movie.imdbRating}` : 'No rating available';
         cardBody.appendChild(rating);
         
-        const heartIcon = `<svg xmlns="https://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16" id=${i}>
+        const heartIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16" id=${i}>
         <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
         </svg>
       `;
@@ -54,14 +54,6 @@ showMovies('Fast And Furious', cardsContainer, 2)
 showMovies('Creed III', cardsContainer, 1);
 showMovies('Rocky IV', cardsContainer, 1)
 showMovies('Thor', cardsContainer, 2)
-const showOutMenu = () => {
-  if(localStorage.length != 0){
-    menu.style.opacity=1;
-  }
-}
-const removeMenu = () => {
-  menu.style.opacity=0;
-}
 
 const btn_search = document.getElementById('btn-search')
 const input_search = document.getElementById('search-input')
