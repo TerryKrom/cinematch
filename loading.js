6332289564a74d59e72c1d087e66613711de6bf8
Guilherme Avila <106833885+TerryKrom@users.noreply.github.com>
2023-07-06 16:52:17 -0300

    Update loading.js

--- loading.js
+++ loading.js
@@ -12,3 +12,32 @@ window.addEventListener('load', function() {
       content.classList.add('visible');
     }, 2000); // Tempo de exibição do GIF de loading em milissegundos (aqui definido como 2 segundos)
   });
+  let icon = document.getElementById('perfil')
+  let a = document.getElementById('a-perfil')
+  let out = document.querySelectorAll('.out')
+        
+        
+if(localStorage.getItem('User') && localStorage.getItem('foto') == false){
+    icon.src='perfil.png'
+    icon.classList.toggle('logged');
+      a.href='account.html'
+      out.forEach(element => {
+          element.style.display='flex';
+          element.classList.toggle('insivible')
+      })
+}else if(localStorage.getItem('User') && localStorage.getItem('UserImg')){
+    icon.src=localStorage.getItem('UserImg')
+    icon.classList.toggle('logged');
+      a.href='account.html'
+      out.forEach(element => {
+          element.style.display='flex';
+          element.classList.toggle('insivible')
+
+      })
+}
+
+if(localStorage.length == 0){
+  out.forEach(element => {
+    element.style.display='none'
+    })
+}
