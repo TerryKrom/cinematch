eacd1c3cfe49cec435cae377250b6b4f590e5572
TerryKrom <terrykrom1@gmail.com>
2023-11-03 13:23:39 -0300

    Code organized

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # CineMatch
+## Your cinema, your way!
+
 > A movie Website, with recomendations and a lot of movie banners!
 
 > in progress...
---
+++ assets/js/loading.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function() {
+    var loading = document.getElementById('loading');
+    var content = document.getElementById('content');
+  
+    // Adiciona a classe "hidden" à div do conteúdo
+    content.classList.add('hidden');
+  
+    // Remove a classe "hidden" quando o conteúdo estiver completamente carregado
+    window.setTimeout(function() {
+      loading.style.display = 'none';
+      content.classList.remove('hidden');
+      content.classList.add('visible');
+    }, 2000); // Tempo de exibição do GIF de loading em milissegundos (aqui definido como 2 segundos)
+  });
+  let icon = document.getElementById('perfil')
+  let a = document.getElementById('a-perfil')
+  let out = document.querySelectorAll('.out')
+  let btn_login = document.querySelectorAll('.btn-login')
+        
+if(localStorage.getItem('User') && localStorage.getItem('foto') == false){
+    icon.src='perfil.png'
+    icon.classList.toggle('logged');
+      a.href='account.html'
+      out.forEach(element => {
+          element.style.display='flex';
+          element.classList.toggle('insivible')
+      })
+}else if(localStorage.getItem('User') && localStorage.getItem('UserImg')){
+    icon.src=localStorage.getItem('UserImg')
+    icon.classList.toggle('logged');
+      a.href='account.html'
+      out.forEach(element => {
+          element.style.display='flex';
+          element.classList.toggle('insivible')
+
+      })
+    btn_login.forEach(e => {
+      e.style.display="none"
+    })
+}
+
+if(localStorage.length == 0){
+  out.forEach(element => {
+    element.style.display='none'
+    })
+}
